69fd2157cf218e6e49bb07100f44da842f78f5e9
leonkae <leon29kae@gmail.com>
2022-05-23 19:43:28 +0300

    modified/upgraded files

---
+++ members/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+'''models'''
+class Members(models.Model):
+    firstname = models.CharField(max_length=255)
+    lastname = models.CharField(max_length=255)
+    
+    
--- myworld/settings.py
+++ myworld/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-]
+    'members.apps.MembersConfig'
+] 
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
